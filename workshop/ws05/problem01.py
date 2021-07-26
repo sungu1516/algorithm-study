5827787851b051b58bb19d2823fae19c3b200db7
sungu <sungu1516@gmail.com>
2021-07-26 22:15:53 +0900

    Add workshop05

--- workshop/ws05/problem01.py
+++ workshop/ws05/problem01.py
@@ -1,9 +1,8 @@
-# 1. 숫자의 의미
+# 1. 평균 점수 구하기
 
-def get_secret_word(num_list):
-    result = ''
-    for num in num_list:
-        result += chr(num)
-    return result
+test = {'q' : 80, 'w' : 90, 'd': 89, 'c':83}
 
-print(get_secret_word([83, 115, 65, 102, 89]))
+def get_dict_avg(dict):
+    return sum(dict.values())/len(dict)
+
+print(get_dict_avg(test))
--- workshop/ws05/problem02.py
+++ workshop/ws05/problem02.py
@@ -1,8 +1,16 @@
-# 2. 내 이름은 몇일까?
-def get_secret_number(word):
-    total = 0
-    for alp in word:
-        total += ord(alp)
-    return total
+# 2. 혈액형 분류하기
 
-print(get_secret_number('tom'))
+blood_list = ['A', 'B', 'A', 'O', 'AB', 'AB', 'O', 'A', 'B', 'O', 'B', 'AB']
+
+def count_blood(blood_types):
+    # 빈 딕셔너리 생성
+    dict = {}
+    # key에 해당하는 개수를 count 메서드를 활용하여 value 로 넣어줌
+    dict['A'] = blood_types.count('A')
+    dict['B'] = blood_types.count('B')
+    dict['O'] = blood_types.count('O')
+    dict['AB'] = blood_types.count('AB')
+
+    return dict
+
+print(count_blood(blood_list))
