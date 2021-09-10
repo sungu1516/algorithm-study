bf284097824b9df2582fc1306111991f76cf7437
sungu <sungu1516@gmail.com>
2021-09-10 23:04:58 +0900

    boj 0910

--- baekjoon/categories/greedy/공유기설치.py
+++ baekjoon/categories/greedy/공유기설치.py
@@ -1,23 +1,23 @@
 N, C = map(int, input().split())    # 집의 개수 N, 공유기의 개수 C
 arr = [int(input()) for _ in range(N)]
-# 집의 위치 1로 표시
-home = [0] * (max(arr)+1)
-for i in arr:
-    home[i] = 1
 
-def binary_search(start, end):
-    middle = (start + end) // 2
+arr.sort()
 
-    if start == middle:                 # 만약 가운데 지점이 없고, 1을 못찾은 경우
-        return 0
+start = 1
+end = arr[-1] - arr[0]
+result = 0
 
-    if home[middle] == 1:               # 1을 찾은 경우 - 해당 인덱스 리턴
-        return middle
-    else:
-        p1 = binary_search(start, middle)    # 중간점의 왼쪽 탐색
-        p2 = binary_search(middle, end)      # 중간점의 오른쪽 탐색
+while(start <= end):
+    mid = (start + end) // 2
+    value = arr[0]
+    count = 1
 
-        return p1 if abs(p1-middle) < abs(p2-middle) else p2 # middle과 가까운 놈을 출력
-
-
-start_p, end_p = min(arr), max(arr)
+    for i in range(1, N):
+        if arr[i] >= value + mid:
+            value = arr[8]      # 공유기 설치 지점을 표시
+            count += 1          # 공유기 개수 추가
+        if count >= C:          # 제시된 공유기 개수 이상을 설치할 수 있는 경우
+            start = mid + 1
+            result = mid
+        else:                   # 제시된 공유기만큼을 설치할 수 없는 경우
+            end = mid - 1
