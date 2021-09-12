d82927d87a934f366427f4886891c13b30d85c13
sungu <sungu1516@gmail.com>
2021-09-12 22:38:39 +0900

    boj 0912

--- baekjoon/categories/greedy/꿀따기.py
+++ baekjoon/categories/greedy/꿀따기.py
@@ -1,30 +1,30 @@
 N = int(input())    # 장소의 수
-honey = list(map(int, input().split()))     # 각 장소의 꿀의 양
+arr = list(map(int, input().split()))     # 각 장소의 꿀의 양
+max_val = 0     # 최대 꿀의 양 초기화
 
-max_val = 2*sum(honey[2:]) if 2*sum(honey[2:]) > 2*sum(honey[0:N-2]) else 2*sum(honey[0:N-2])      # 도착지점이 양쪽 끝인 경우를 먼저 고려해준다.
+# 꿀통이 오른쪽 끝에 있는 경우
+for i in range(1, N-1):     # i : 움직이는 출발점의 위치
+    fixed = sum(arr) - arr[0] - arr[i]
+    move = sum(arr[i+1:])
+    total = fixed + move
+    if total > max_val:
+        max_val = total
 
-for i in range(N-2, 0, -1):    # 도착지점 = i (양 끝 지점 배제)
-    temp = 0
-    left, right = sum(honey[1:i+1]), sum(honey[i:N-1])
-    if left > right:    # 첫 번째 출발 위치가 왼쪽 끝인 경우
-        temp += left    # 첫 번째 출발 위치로부터 도착지까지의 꿀을 모두 더해준다.
+# 꿀통이 왼쪽 끝에 있는 경우
+for i in range(N-2, 0, -1):
+    fixed = sum(arr) - arr[-1] - arr[i]
+    move = sum(arr[:i])
+    total = fixed + move
+    if total > max_val:
+        max_val = total
 
-        # 두 번째 위치 결정
-        if left-honey[1] > right:   # 두 번째 출발 위치가 왼쪽 끝인경우
-            temp += left-honey[0]
+# 꿀통이 가운데 있는 경우 => 결국 최대값이 나오려면 출발점은 양 끝으로 고정됨
+for i in range(1, N-1):     # i : 꿀통의 위치
+    left = sum(arr[1:i+1])
+    right = sum(arr[i:N-1])
+    total = left + right
 
-        else:   # 두 번째 출발 위치가 오른쪽 끝인경우
-            temp += right
-    else:     # 첫 번째 출발 위치가 오른쪽 끝인 경우
-        # 두 번째 위치 결정
-        if left > right - honey[N-2]:  # 두 번째 출발 위치가 왼쪽 끝인경우
-            temp += left
-
-        else:  # 두 번째 출발 위치가 오른쪽 끝인경우
-            temp += right - honey[N-2]
-
-    if max_val < temp:
-        max_val = temp
-
+    if total > max_val:
+        max_val = total
 
 print(max_val)
