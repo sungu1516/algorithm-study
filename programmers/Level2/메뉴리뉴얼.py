3ed2a44410da520fefb6740665fc9a0474834d5b
sungu <sungu1516@gmail.com>
2021-10-22 23:52:44 +0900

    1022 programmers

--- programmers/Level2/메뉴리뉴얼.py
+++ programmers/Level2/메뉴리뉴얼.py
@@ -3,22 +3,33 @@ from itertools import combinations
 def solution(orders, course):
     result = []
     # course 순회하며, 특정 길이의 문자열의 빈도 hash에 저장
-    hash = dict()
     for cnt in course:
+        # hash 초기화
+        hash = dict()
+        
         # orders를 순회
         for order in orders:
             if len(order) < cnt: continue
             
             for comb in combinations(order, cnt):
-                string = ''.join(comb)
+                string = ''.join(sorted(comb))
+
                 hash[string] = hash.get(string, 0) + 1
-                
-            
-        # hash 완성    
-        # cnt와 동일한 길이를 가진 order에 대해서만 결과에 누적
+                  
+        # hash 완성
+        # 만약 hash가 비어있다면 - continue
+        if not hash: continue
+        
+        # hash의 value 중 max_value를 찾고, 이에 대응하는 key(s) 을 result에 담기
+        max_value = max(hash.values())
+        
+        # 최소 2명 이상의 손님으로부터 주문되어야 
+        if max_value < 2: continue
+        
         for key, value in hash.items():
-            if value == cnt:
-                print(value)
-                result.append(''.join(sorted(key)))
-        print(hash)
+            if value == max_value:
+                result.append(''.join(key))      # 정렬해서 넣어준다.
+        
+    # 알파벳순으로 정렬
+    result.sort()
     return result
