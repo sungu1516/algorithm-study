6833b97e1ddf215345170ffab47e5ec21b556ea0
sungu <sungu1516@gmail.com>
2021-12-02 20:11:08 +0900

    programmers 1202

--- programmers/Level2/전화번호목록.py
+++ programmers/Level2/전화번호목록.py
@@ -1,21 +1,23 @@
 def solution(phone_book):
-    N = len(phone_book)     # 전화번호부 안의 전화번호의 개수
-    for i in range(N-1):
-        pre_len = len(phone_book[i])      # 현재 가리키고 있는 전화번호의 길이
-        
-        for j in range(i+1, N):     # 중복 비교하지 않게, 현재 전화번호의 뒤의 것들과만 비교   
-            post_len = len(phone_book[j])
-            
-            # 만약 비교대상이 되는 전화번호의 길이가 더 짧다면
-            if pre_len > post_len:
-                i, j = j, i
-                pre_len, post_len = post_len, pre_len
-            
-            for k in range(post_len - pre_len + 1):
-                print(phone_book[i])
-                print(phone_book[j][k:k+pre_len])
-                if phone_book[i] == phone_book[j][k:k+pre_len]:
-                    return False
-                
-    return True
-            
+    '''
+    Review
+    문제를 잘못이해했다.. 접두가 일치하는 경우에 대해서만 고려해야 하는데, 위치를 고려하지 않고 풀었다.
+    처음엔 이중 for문으로 구현했더니, 정확성은 통과했지만 효율성 중 2개의 케이스를 통과하지 못했다.
+    '''
+    
+    '''
+    Approach
+    우선 배열을 숫자의 길이를 기준으로 정렬한다.
+    이후, 앞의 숫자부터 차례대로 비교하며 접두어인 경우를 확인한다.
+    길이를 기준으로 짧은 숫자들이 앞에 위치하게 되므로, 뒤에 있는 숫자들에 대해서만 비교하면 된다.
+    '''
+    
+    phone_book = sorted(phone_book)
+    answer = True
+    
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            answer = False
+            break
+    
+    return answer
