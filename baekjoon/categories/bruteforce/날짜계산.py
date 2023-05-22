f7d707ed1f60eb3921578b42308d9ed7c42b6190
sungu <sungu1516@gmail.com>
2023-05-23 00:27:50 +0900

    boj 23.05.23

---
+++ baekjoon/categories/bruteforce/날짜계산.py
@@ -0,0 +1,23 @@
+# input
+E, S, M = map(int, input().split())
+
+# main
+year = 1
+e, s, m = 1, 1, 1
+
+while True:
+    if E == e and S == s and M == m:
+        print(year)
+        break
+
+    e += 1
+    s += 1
+    m += 1
+    year += 1
+
+    if e == 16:
+        e = 1
+    if s == 29:
+        s = 1
+    if m == 20:
+        m = 1
--- baekjoon/categories/bruteforce/리모컨.py
+++ baekjoon/categories/bruteforce/리모컨.py
@@ -1,35 +1,37 @@
-# 입력값
-ch = int(input())
-n = int(input())
-btnArr = list(map(int, input().split()))    # 고장난 버튼 배열
+# input
+import sys
+input = sys.stdin.readline
+targetNum = int(input())
+m = int(input())
+broken = list(map(int, input().split()))
 
-# dfs
-def dfs(strNum):
-    global min_cnt
-    # 종료조건
-    if len(strNum) == len(str(ch)):
-        # 버튼 누르는 횟수 구하기
-        cnt = abs(int(strNum) - ch) + len(strNum)
-        # 갱신
-        if cnt < min_cnt:
-            min_cnt = cnt
-    # 목표한 채널의 자리수와 동일한 경우
-    pass
+# function
+def isPossible(num):
+    if num == 0:
+        if 0 in broken:
+            return 0
+        else:
+            return 1
+
+    length = 0
+    while num > 0:
+        remain = num % 10
+        if remain in broken:
+            return 0
+
+        num //= 10
+        length += 1
+
+    return length
 
 # main
-min_cnt = ch # 최소 횟수 초기화
-tmpCh = ''
+minVal = abs(targetNum - 100)   # 초기값
+for num in range(1000001):
+    res = isPossible(num)
 
-# 목표 채널의 각 자릿수를 순회
-for strNum in str(ch):
-    # 해당 자리수 버튼이 고장나지 않은 경우
-    if strNum not in btnArr:
-        dfs(tmpCh + strNum)
-        continue
+    # 해당 번호로 이동 가능한 경우
+    if res > 0:
+        if minVal > res + abs(targetNum - num):
+            minVal = res + abs(targetNum - num)
 
-    # 해당 자리수 버튼이 고장난 경우
-    # 재귀 호출 - 0 ~ 9 숫자 중 고장나지 않은 숫자인 경우
-    for i in range(9):
-        if i not in btnArr:
-            # 현재까지 완성된 자리수를 인자로 넣어준다.
-            dfs(tmpCh + str(i))
+print(minVal)
